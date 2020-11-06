2c9df9e5298f058378390ce92c1702bed9f661d6
IbrahimSyouri <70780875+IbrahimSyouri@users.noreply.github.com>
2020-11-06 10:03:57 +0200

    Ib

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@
   // Preloader
   $(window).on('load', function() {
     if ($('#preloader').length) {
-      $('#preloader').delay(100).fadeOut('slow', function() {
+      $('#preloader').delay(10).fadeOut('slow', function() {
         $(this).remove();
       });
     }
@@ -137,7 +137,7 @@
   // Init AOS
   function aos_init() {
     AOS.init({
-      duration: 1000,
+      duration: 100,
       once: true
     });
   }
